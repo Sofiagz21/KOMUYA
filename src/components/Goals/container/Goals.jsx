0ea54e7d934950724fef5a7f5263c911929dac92
Komuya <komuyaproyecto2021@gmail.com>
2021-08-28 21:06:45 -0500

    Realice la parte de los retos -Alejandra

--- src/components/Goals/container/Goals.jsx
+++ src/components/Goals/container/Goals.jsx
@@ -1,14 +1,43 @@
 import React from 'react'
 import {Link} from "react-router-dom"
-
-
+import '../stylesGoals.css'
+import '../../stylesButtons.css'
 
 const Goals = () => {
     return (
-        <div>
-            <h1>Hola</h1>
+        <div className="goals">
+            <p className="titulo">Retos de la semana</p>
+            <div className="container">
+              <div className="retos">
+                <div className="Lunes">
+                <div class="contentCards">
+                <div class="card">
+                    <div class="imgBx">
+                    </div>
+                        <div class="content">
+                            <div>
+                                <h3 id="Lunes">Mora</h3>
+                            </div>
+                    </div>
+            </div>
+            </div>
+            </div>
+                <div className="Martes">
+                   <p>Hola</p>
+                </div>
+                <div className="Miercoles">
+                  
+                </div>
+                <div className="Jueves">
+
+                </div>
+                <div className="Viernes">
+
+                </div>
+                </div>
+            </div>
             <Link to="/">
-              <button> Me voy para Home </button>
+              <button className="regresar"> Me voy para Home </button>
             </Link>
         </div>
     )
