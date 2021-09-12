056901d87effa4869c671f4bceb7e3461446ab83
Komuya <komuyaproyecto2021@gmail.com>
2021-09-11 22:10:59 -0500

    Hice el diseño del primer y tercer reto -Alejandra

--- src/components/Felicitaciones/Fel3.jsx
+++ src/components/Felicitaciones/Fel3.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Fel3 = () => {
     return (
-        <div>
-            
+        <div className="containerFel">
+        <div className="cardFeli">
+            <div className="textCard">
+             <h3>¡Felicidades!</h3>
+             <p>Has finalizado el reto del día Miercoles.</p>
+             <p>Vuelve mañana.</p>
+         </div>
         </div>
+     </div>
     )
 }
 
--- src/pages/Challenges/ChallengeOne/container/challengeOne.jsx
+++ src/pages/Challenges/ChallengeOne/container/challengeOne.jsx
@@ -1,31 +1,7 @@
 import React from 'react'
 import Encabezado from '../../../../components/Encabezado/container/encabezado'
+import Question from '../../../../utils/images/Encabezado/question.svg'
 import '../container/styles.css'
-
-
-
-function onMyLoad(){
-	oneWord()
-	twoWord()
-}/*FINALIZACIÓN DE LA FUNCIÓN QUE ME CARGA EN EL NAVEGADOR*/
-
-
-/*FUNCIÓN QUE ME PERMITE ELEGIR EL PRIMER SIGNO*/
-function oneWord() {
-let primerPalabra=['Aventurera', 'Extrovertida', 'Introvertida','Amigable', 'Solitaria',];
-for ( var i in primerPalabra){
-    
-document.getElementById("palabraOne").innerHTML += "<option value='"+primerPalabra[i]+"'>"+primerPalabra[i]+"</option>";
-}
-}
-    
-function twoWord() {
-     let segundaPalabra=['Capricornio', 'Acuario', 'Piscis','Aries', 'Tauro', 'Geminis', 'Cancer', 'Leo', 'Virgo', 'Libra','Escorpio', 'Sagitario'];
-    for ( var i in segundaPalabra){
-        
-    document.getElementById("segundaPalabra").innerHTML += "<option value='"+segundaPalabra[i]+"'>"+segundaPalabra[i]+"</option>";
-           }
-    }
 
 
 const ChallengeOne= () => {
@@ -39,14 +15,29 @@ const ChallengeOne= () => {
                     <p>El reto del dia de hoy es conocerte, selecciona dos palabras que te identifique y reconoce que tipo de persona eres.</p>
                 </div>
                     <div className="seleccionar">
-                         <select name="primerPalabra" id="palabraOne">
-						    <option class="opcion" value=""> Personalidad </option>
-					    </select>
-                    <select name="segundaPalabra" id="palabraTwo">
-						<option class="opcion" value="">Objeto </option>
-					</select>
+                     <div className="options">
+                        <div className="personality">
+                            <select name="primerPalabra" id="palabraOne">
+                                <option class="opcion" value=""> Personalidad </option>
+                            </select>
+                        </div>
+                    <div className="object">
+                        <select name="segundaPalabra" id="palabraTwo">
+                            <option class="opcion" value="">Objeto </option>
+                        </select>
                     </div>
-        </div>
+                    </div>
+                    </div>
+                    <div className="question">
+                     <img src={Question}/>
+                    </div>
+
+                    <div className="answer">
+                        <div class="result">
+                            <h6 id="resultadito"> </h6>
+                        </div>
+                    </div>
+                    </div>
     )
 }
 export default ChallengeOne
