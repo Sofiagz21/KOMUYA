35ed868f1807f43d5a6daed0b0068c9924a3ad9c
KOMUYA <komuyaproyecto2021@gmail.com>
2021-09-18 23:10:13 -0500

    fix

--- src/pages/Challenges/ChallengeTwo/container/challengeTwo.jsx
+++ src/pages/Challenges/ChallengeTwo/container/challengeTwo.jsx
@@ -1,13 +1,46 @@
 import React from 'react'
 import Encabezado from '../../../../components/Encabezado/container/encabezado'
-import Contenedor from '../../ChallengeTwo/container/challengeTwoContainer'
 import '../container/styles.css'
+import {Link} from "react-router-dom"
 
 const ChallengeTwo= () => {
     return (
-        <div className="containerTwo">
+        <div className="container">
              <Encabezado/>
-             <Contenedor/>
+             <div className="containerChallengeTwo">
+                <div className="title_Two">
+                <h1>El reto del día  es conocerte es conocerte, nombra 10 cosas que te gusten de ti <i class="fa fa-check" aria-hidden="true"></i></h1>
+                </div>
+            <div className="boxTwo">
+                <form action="">
+                    <ol>
+                        <li> <input type="text" name="nombre" required autoComplete="off"/></li>
+                        <li> <input type="text" name="nombre" required autoComplete="off"/></li>
+                        <li> <input type="text" name="nombre" required autoComplete="off"/></li>
+                        <li> <input type="text" name="nombre" required autoComplete="off"/></li>
+                        <li> <input type="text" name="nombre" required autoComplete="off"/></li>
+                        <li> <input type="text" name="nombre" required autoComplete="off"/></li>
+                        <li> <input type="text" name="nombre" required autoComplete="off"/></li>
+                        <li> <input type="text" name="nombre" required autoComplete="off"/></li>
+                        <li> <input type="text" name="nombre" required autoComplete="off"/></li>
+                        <li> <input type="text" name="nombre" required autoComplete="off"/></li>
+                        <li> <input type="text" name="nombre" required autoComplete="off"/></li>
+                    </ol>
+                </form>
+            </div>
+            <div className="enviarTwo">
+                <Link to="/Feli2">
+                    <button className="sendTwo">
+                        Enviar
+                    </button>
+                </Link>
+            </div>
+
+
+
+
+
+             </div>
         </div>
     )
 }
